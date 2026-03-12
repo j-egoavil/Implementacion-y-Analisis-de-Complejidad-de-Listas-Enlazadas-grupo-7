745615ec6f3e90115ad6cf365c49e3c32176ce22
Torito9609 <toroandres85@gmail.com>
2026-03-12 13:33:06 -0500

    Se crea constructor para SinglyNode, se crean get y set methods para getNext, setNext y setValue

--- src/main/java/list/DoublyNode.java
+++ src/main/java/list/DoublyNode.java
@@ -4,7 +4,8 @@ public class DoublyNode<T> implements Position<T>{
     T data;
     DoublyNode<T> next;
     DoublyNode<T> prev;
-    
+
+
     @Override
     public T getValue() {
         return null;
--- src/main/java/list/SinglyNode.java
+++ src/main/java/list/SinglyNode.java
@@ -1,11 +1,27 @@
 package list;
 
 public class SinglyNode<T> implements Position<T>{
-    T data;
+    T value;
     SinglyNode<T> next;
+
+    public SinglyNode(T value){
+        this.value = value;
+    }
 
     @Override
     public T getValue() {
         return null;
+    }
+
+    public SinglyNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(SinglyNode<T> next) {
+        this.next = next;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
     }
 }
