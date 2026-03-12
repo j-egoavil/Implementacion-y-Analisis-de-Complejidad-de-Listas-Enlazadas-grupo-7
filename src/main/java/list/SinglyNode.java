dbdf78dd887fe53039e80d56e1f9a79c8782f5c4
Torito9609 <toroandres85@gmail.com>
2026-03-12 13:27:45 -0500

    se crea la clase SinglyNode para implementacion de listas simplemente enlazadas, no se declara constructor ya que se aprovechara el constructor por defecto

---
+++ src/main/java/list/SinglyNode.java
@@ -0,0 +1,11 @@
+package list;
+
+public class SinglyNode<T> implements Position<T>{
+    T data;
+    SinglyNode<T> next;
+
+    @Override
+    public T getValue() {
+        return null;
+    }
+}
