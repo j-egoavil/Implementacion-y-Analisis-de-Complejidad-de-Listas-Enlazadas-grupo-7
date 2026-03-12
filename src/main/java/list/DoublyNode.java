da96624c7c84fcefc3cddd02bd843eb82b9f36a2
Torito9609 <toroandres85@gmail.com>
2026-03-12 13:37:09 -0500

    se crean constructor y metodos set y get para DoublyNode, correcto encapsulamiento

--- src/main/java/list/DoublyNode.java
+++ src/main/java/list/DoublyNode.java
@@ -1,13 +1,38 @@
 package list;
 
 public class DoublyNode<T> implements Position<T>{
-    T data;
+    T value;
     DoublyNode<T> next;
     DoublyNode<T> prev;
 
+    public DoublyNode(T value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
 
     @Override
     public T getValue() {
         return null;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public DoublyNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(DoublyNode<T> next) {
+        this.next = next;
+    }
+
+    public DoublyNode<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(DoublyNode<T> prev) {
+        this.prev = prev;
     }
 }
