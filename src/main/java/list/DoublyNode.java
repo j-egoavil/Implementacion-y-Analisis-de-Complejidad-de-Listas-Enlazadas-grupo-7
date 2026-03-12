242407a001cc97c9b0360e646d85a53b2db03e5b
Torito9609 <toroandres85@gmail.com>
2026-03-12 13:29:30 -0500

    se implementa la clase DoublyNode para implementación de listas doblemente enlazadas, se aprovecha el constructor por defecto

---
+++ src/main/java/list/DoublyNode.java
@@ -0,0 +1,12 @@
+package list;
+
+public class DoublyNode<T> implements Position<T>{
+    T data;
+    DoublyNode<T> next;
+    DoublyNode<T> prev;
+    
+    @Override
+    public T getValue() {
+        return null;
+    }
+}
