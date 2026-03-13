dfd57b55315d47f6bb270f29dbbc15d3b9f1c557
Torito9609 <toroandres85@gmail.com>
2026-03-13 11:01:57 -0500

    por decisiones de diesño y encapsulamiento las clase de Node y DoublyNode se implementaran con clases privadas estaticas dentro de su respectiva list que los necesite

---
+++ src/main/java/list/SinglyLinkedList.java
@@ -0,0 +1,80 @@
+package list;
+
+public class SinglyLinkedList<T> implements List<T> {
+    private SinglyNode<T> next;
+    private int size;
+
+    private static class SinglyNode<T> implements Position<T>{
+        private T value;
+        private SinglyNode<T> next;
+
+        public SinglyNode(T value){
+            this.value = value;
+        }
+
+        @Override
+        public T getValue() {
+            return value;
+        }
+    }
+
+    @Override
+    public void pushFront(T value) {
+
+    }
+
+    @Override
+    public void pushBack(T value) {
+
+    }
+
+    @Override
+    public T popFront() {
+        return null;
+    }
+
+    @Override
+    public T popBack() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public T topFront() {
+        return null;
+    }
+
+    @Override
+    public T topBack() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Position<T> find(T value) {
+        return null;
+    }
+
+    @Override
+    public void erase(Position<T> position) {
+
+    }
+
+    @Override
+    public void addBefore(Position<T> position, T value) {
+
+    }
+
+    @Override
+    public void addAfter(Position<T> position, T value) {
+
+    }
+}
